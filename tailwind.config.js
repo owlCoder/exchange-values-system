f12e537e399cb7ec6fb0c438d259b1c145ba3a60
Danijel Jovanović <danijel.xda@gmail.com>
2023-11-10 11:18:46 +0100

    prepare fonts, css, and templates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: "var(--main)",
+        "second-text": "var(--second-text)",
+        white: "var(--white)",
+      },
+      fontFamily: {
+        "16-PX-normal-text": "var(--16-PX-normal-text-font-family)",
+        title: "var(--title-font-family)",
+      },
+    },
   },
   plugins: [],
-}
+};
