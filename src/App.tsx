8b4688913a6619ea5eed7ad837082d1ba8e7738a
Danijel Jovanović <danijel.xda@gmail.com>
2023-11-10 14:34:46 +0100

    Update project structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,4 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+function App(): JSX.Element {
   return (
     <div className="App">
       <h1 className='linear-text'>JovanovićJovanović Jovanović</h1>
