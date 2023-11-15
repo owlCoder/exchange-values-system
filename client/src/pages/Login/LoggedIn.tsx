b2208b152bd97b56c661339c04cdb35ba7ade07b
Danijel Jovanović <danijel.xda@gmail.com>
2023-11-15 19:51:33 +0100

    Keep commits up

--- client/src/pages/Login/LoggedIn.tsx
+++ client/src/pages/Login/LoggedIn.tsx
@@ -36,8 +36,7 @@ const LoggedIn = () => {
                 <button
                   type="submit"
                   className="w-full text-white bg-rose-700 hover:bg-rose-800 focus:ring-4 focus:outline-none focus:ring-rose-400 rounded-lg text-md font-semibold px-5 py-2.5 text-center dark:bg-rose-800 dark:hover:bg-rose-700 dark:focus:ring-rose-800"
-                  onClick={handleLogout}
-                >
+                  onClick={handleLogout} >
                   Sign Out <LiaSignOutAltSolid className="inline ml-1 text-xl -mt-1" />
                 </button>
               </form>
