633b0face7463a8714739ab9abee1f7b514cbc73
Danijel Jovanović <danijel.xda@gmail.com>
2023-11-19 17:10:42 +0100

    Controllers: Add docs
    Co-Authored-By: Luchi2431 <142620761+Luchi2431@users.noreply.github.com>
    Co-Authored-By: DoraMitic <116057029+DoraMitic@users.noreply.github.com>
    Co-Authored-By: Vuk Ognjanovic <56590132+vukajlo01@users.noreply.github.com>

--- api/controllers/token.py
+++ api/controllers/token.py
@@ -4,6 +4,21 @@ from models.token import Token
 
 # Method to create a new token in database and create a new session
 def create_token(token, user_email, user_uid):
+    """
+    Create a new token in the database and create a new session.
+
+    Args:
+        token (str): The token string to be stored.
+        user_email (str): Email associated with the user.
+        user_uid (int): User ID associated with the token.
+
+    Returns:
+        bool: True if the token is successfully created and session created, False otherwise.
+
+    Example:
+        result = create_token('sample_token_string', 'user@example.com', 1)
+        print(result)  # True or False
+    """
     try:
         new_token = Token(token = token, email = user_email, uid = user_uid)
         db.session.add(new_token)
@@ -15,6 +30,21 @@ def create_token(token, user_email, user_uid):
 
 # Method to check is token still valid
 def is_token_valid(token, uid):
+    """
+    Check if the token is still valid for a specific user ID.
+
+    Args:
+        token (str): The token string to be checked.
+        uid (int): The user ID.
+
+    Returns:
+        tuple: A tuple containing (validity_status, user_verified_status). 
+               Returns (False, False) if an error occurs.
+
+    Example:
+        validity, verified = is_token_valid('sample_token_string', 1)
+        print(validity, verified)  # True/False for token validity and user verified status
+    """
     try:
         token_entry = db.session.query(Token).filter(Token.token == token, Token.uid == uid).first()
         if token_entry:
@@ -28,6 +58,19 @@ def is_token_valid(token, uid):
     
 # Method to delete token from database
 def delete_token(token):
+    """
+    Delete a token from the database.
+
+    Args:
+        token (str): The token string to be deleted.
+
+    Returns:
+        bool: True if the token is found and successfully deleted, False otherwise.
+
+    Example:
+        result = delete_token('sample_token_string')
+        print(result)  # True or False
+    """
     try:
         token_entry = db.session.query(Token).filter(Token.token == token).first()
         if token_entry:
