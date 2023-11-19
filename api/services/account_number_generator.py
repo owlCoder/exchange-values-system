bb914749eebe241b425f28fe46089abcc2e8e64f
Danijel Jovanović <danijel.xda@gmail.com>
2023-11-19 18:08:49 +0100

    Docs
    Co-Authored-By: Luchi2431 <142620761+Luchi2431@users.noreply.github.com>
    Co-Authored-By: DoraMitic <116057029+DoraMitic@users.noreply.github.com>
    Co-Authored-By: Vuk Ognjanovic <56590132+vukajlo01@users.noreply.github.com>

--- api/services/account_number_generator.py
+++ api/services/account_number_generator.py
@@ -1,6 +1,16 @@
 import random
 
 def generate_account_number():
+    """
+    Generates a random account number.
+
+    This function generates a unique account number by combining a prefix from the provided list
+    and a randomly generated sequence of digits.
+
+    Returns:
+        str: A randomly generated account number in the format "{prefix}-{random_digits}".
+             Example: "170-1234567890123"
+    """
     # Choose a prefix from the provided list
     prefixes = [170, 180, 182, 210, 225, 270, 299, 360, 365]
     prefix = str(random.choice(prefixes))
--- api/services/password_hasher.py
+++ api/services/password_hasher.py
@@ -2,5 +2,17 @@ import hashlib
 
 # Basic hash method to store hashed passwords
 def hash_method(password):
+    """
+    Hashes the provided password using the SHA-256 algorithm.
+
+    Args:
+        password (str): The password to be hashed.
+
+    Returns:
+        str: The hashed password as a hexadecimal string.
+
+    Example:
+        hashed_password = hash_method("my_password123")
+    """
     # Hash the password using SHA-256
     return hashlib.sha256(password.encode()).hexdigest()
