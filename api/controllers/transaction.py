869a9f2e889aa133c86adb6f9b4993bfb43b5bcd
Danijel Jovanović <danijel.xda@gmail.com>
2023-11-25 12:09:12 +0100

    Transactions: Rewrite processing method
    Co-Authored-By: DoraMitic <116057029+DoraMitic@users.noreply.github.com>

--- api/controllers/transaction.py
+++ api/controllers/transaction.py
@@ -56,80 +56,63 @@ def create_transaction(sender_uid, sender_account_id, amount, receiver_account_n
         db.session.rollback()
         return False
 
-def get_transactions_by_approved_status(status):
-    transcations = db.session.query(Transactions).filter(Transactions.approved=='ON HOLD').all()
-    return transcations
-
-def update_transcation_approved_status(transaction_id, status):
-    try:
-        transaction = db.session.query(Transactions).get(transaction_id)
-
-        if transaction:
-            transaction.approved = status
-            db.session.commit()
-    except Exception as e:
-        db.session.rollback()
-
 # Method to process transactions waiting for proccessing
 def process_on_hold_transactions():
     # Get all on hold transactions
-    try:
-        transactions = get_transactions_by_approved_status("ON HOLD")
+        transactions = db.session.query(Transactions).filter(Transactions.approved=='ON HOLD').all()
 
         if transactions:
             for transaction in transactions:
-                if process_transaction(transaction.sender_account_id, transaction.receiver_account_number, transaction.amount):
-                    update_transcation_approved_status(transaction.id, "APPROVED")
-                else:
-                    update_transcation_approved_status(transaction.id, "DENIED")
+                sender_account = get_current_account_by_id(transaction.sender_account_id)
+                receiver_account = get_account_by_number(transaction.receiver_account_number)
+                    
+                # Check if current accounts exist and does sender has enough balance to send
+                if sender_account is None or receiver_account is None or sender_account.balance < transaction.amount:
+                    transaction.approved = "DENIED"
+                    continue
 
-                print(transaction.approved)
-                live_update = jsonify({'data': transaction})
+                # Check if receiver has current account with currency from sender's account
+                account_id_in_receiver_currency = get_account_by_number_and_currency(receiver_account.account_number, sender_account.currency)
 
-                # Emit transaction status update
-                socketio.emit('updated_data', live_update, namespace="/api/realtime")
+                try:
+                    if account_id_in_receiver_currency is None:
+                        new_account = CurrentAccount(
+                        account_number=receiver_account.account_number,
+                        balance=transaction.amount,
+                        currency=sender_account.currency,
+                        card_number=receiver_account.card_number,
+                        uid=receiver_account.uid
+                        )
+                        db.session.add(new_account)
+                    else:
+                        account = db.session.query(CurrentAccount).get(receiver_account.account_id)
+                        account.balance = account.balance + Decimal(str(transaction.amount))
+                    
+                    # Update sender's account balance
+                    account = db.session.query(CurrentAccount).get(transaction.sender_account_id)
+                    account.balance = account.balance - Decimal(str(transaction.amount))
+
+                    # Update transaction status
+                    transaction.approved = "APPROVED"
+                    
+                    # Send email to sender and receiver about transactions approval
+                    sender_message = transaction_message(transaction.amount, sender_account.currency, sender_account.account_number, "Out", sender_account.balance)
+                    receiver_message = transaction_message(transaction.amount, sender_account.currency, transaction.receiver_account_number, 'In', get_current_account_by_id(account_id_in_receiver_currency).balance)
+                    sender = get_user_by_id(sender_account.uid)
+                    receiver = get_user_by_id(receiver_account.uid)
+
+                    prepare(sender["email"], sender_message, "Transcation has been processed")
+                    prepare(receiver["email"], receiver_message, "Transcation has been processed")
+
+                    print("---------------------------------------------------------------")
+                    print(str(transaction.id) + " " + transaction.approved)
+                    print("---------------------------------------------------------------")
+                    live_update = jsonify({'data': transaction.serialize()})
+
+                    # Emit transaction status update
+                    socketio.emit('updated_data', live_update, namespace="/api/realtime")
                 
-                db.session.commit()
-    except Exception as e:
-        return
-
-# Method to process individual transaction
-def process_transaction(sender_account_id, receiver_account_number, amount):
-    sender_account = get_current_account_by_id(sender_account_id)
-    receiver_account = get_account_by_number(receiver_account_number)
-    
-    # Check if current accounts exist and does sender has enough balance to send
-    if sender_account is None or receiver_account is None or sender_account.balance < amount:
-        return False
-    
-    # Check if receiver has current account with currency from sender's account
-    account_id_in_receiver_currency = get_account_by_number_and_currency(receiver_account.account_number, sender_account.currency)
-
-    # Create new currency for receiver account
-    if account_id_in_receiver_currency is None:
-        success = create_current_account(receiver_account.account_number, amount, receiver_account.currency, receiver_account.card_number, receiver_account.uid)
-    else:
-        # Update receiver's existing account balance
-        success = update_account_balance(account_id_in_receiver_currency, amount)
-    
-    # Receiver has new transaction approved
-    if success:
-        update_account_balance(sender_account_id, -amount)
-
-        # Send email to sender and receiver about transactions approval
-        sender_message = transaction_message(amount, sender_account.currency, sender_account.account_number, "Out", sender_account.balance)
-        receiver_message = transaction_message(amount, sender_account.currency, receiver_account_number, 'In', get_current_account_by_id(account_id_in_receiver_currency).balance)
-
-        # Send transactions confirmation emails
-        sender = get_user_by_id(sender_account.uid)
-        receiver = get_user_by_id(receiver_account.uid)
-        return True
-        if sender and receiver and \
-           prepare(sender.email, sender_message, "Transcation has been processed") and \
-           prepare(receiver.email, receiver_message, "Transcation has been processed"):
-                
-            return True
-        else:
-            return False
-    else:
-        return False
+                    db.session.commit()
+                except Exception as e:
+                    print(str(e))
+                    db.session.rollback()
