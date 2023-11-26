abc292cd57bdab66c0f015c189453b8dbc2cef8a
Danijel Jovanović <danijel.xda@gmail.com>
2023-11-26 13:06:36 +0100

    Dockerize

--- api/index.py
+++ api/index.py
@@ -3,4 +3,4 @@ from config.socket import socketio
 
 if __name__ == '__main__':
     app = create_app()
-    socketio.run(app, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
