76232abd4e796e1af09afb8ad0cb572a2dab8b94
Danijel Jovanović <danijel.xda@gmail.com>
2023-11-26 23:11:18 +0100

    Update currencies service
    Co-Authored-By: DoraMitic <116057029+DoraMitic@users.noreply.github.com>
    Co-Authored-By: Vuk Ognjanovic <56590132+Vukajlo01@users.noreply.github.com>
    Co-Authored-By: Luchi2431 <142620761+Luchi2431@users.noreply.github.com>

--- api/routes/currencies.py
+++ api/routes/currencies.py
@@ -6,10 +6,10 @@ currencies_blueprint = Blueprint('currencies_blueprint', __name__)
 class CurrenciesBlueprintDocumentation:
     @currencies_blueprint.route('/api/currencies', methods=['GET'])
     def get_available_currencies():
-        currencies = GetAvailableCurrencies()
+        currencies = get_available_currencies()
         return jsonify(currencies), 200
 
     @currencies_blueprint.route('/api/currencies/course', methods=['GET'])
     def get_available_currencies_course():
-        currencies_course = GetAvailableCurrenciesCourse()
+        currencies_course = get_available_currencies_course()
         return jsonify(currencies_course), 200
--- api/services/currencies_service.py
+++ api/services/currencies_service.py
@@ -5,6 +5,7 @@ from services.caching_service import read_cached_data, write_cached_data, is_cac
 
 STATIC_FOLDER = "static"
 
+
 def get_available_currencies(force_refresh=False):
     cached_file = os.path.join(STATIC_FOLDER, "available_currencies.json")
 
@@ -31,3 +32,23 @@ def get_available_currencies_course(force_refresh=False):
     write_cached_data(cached_file, data)
 
     return {currency["code"]: currency for currency in data["currencies"]}
+
+
+def convert_currency(amount, from_currency, to_currency):
+    exchange_rates = get_available_currencies_course()
+
+    if not isinstance(exchange_rates, dict):
+        return -1
+
+    from_rate_data = exchange_rates.get(from_currency)
+    to_rate_data = exchange_rates.get(to_currency)
+
+    from_rate = float(from_rate_data.get("course").replace(",", ".")) / float(
+        from_rate_data.get("number")
+    )
+    to_rate = float(to_rate_data.get("course").replace(",", ".")) / float(
+        to_rate_data.get("number")
+    )
+
+    converted = from_rate * float(amount) / to_rate
+    return round(converted, 4)
