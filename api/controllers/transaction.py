cddc84fba11a12585708612eabb7616a0f2d1dde
Danijel Jovanović <danijel.xda@gmail.com>
2023-11-26 14:28:37 +0100

    Update SQL, drop swagger support

--- api/controllers/transaction.py
+++ api/controllers/transaction.py
@@ -1,6 +1,4 @@
 import json
-import traceback
-from flask import current_app, jsonify
 from models.transaction import Transactions
 from config.database import db
 from controllers.current_account import *
@@ -11,34 +9,6 @@ from config.socket import socketio
 
 # Function to create a new transaction record using db.session
 def create_transaction(sender_uid, sender_account_id, amount, receiver_account_number, receiver_email, receiver_name, receiver_surname, approved):
-    """
-    Creates a new transaction record in the database.
-
-    Args:
-        sender_uid (int): The user ID of the sender.
-        sender_account_id (int): The account ID of the sender.
-        amount (Decimal): The amount involved in the transaction.
-        receiver_account_number (str): The account number of the receiver.
-        receiver_email (str): Email of the receiver.
-        receiver_name (str): Name of the receiver.
-        receiver_surname (str): Surname of the receiver.
-        approved (str): Status of the transaction - 'ON HOLD', 'APPROVED', or 'DENIED'.
-
-    Returns:
-        bool: True if the transaction is successfully created, False otherwise.
-
-    Example:
-        create_transaction(
-            sender_uid=1,
-            sender_account_id=123,
-            amount=Decimal('100.00'),
-            receiver_account_number=170-48909648905,
-            receiver_email="receiver@example.com",
-            receiver_name="Receiver",
-            receiver_surname="Surname",
-            approved="ON HOLD"
-        )
-    """
     try:
         new_transaction = Transactions(
             sender_uid=sender_uid,
--- api/services/auth_service.py
+++ api/services/auth_service.py
@@ -5,19 +5,6 @@ from services.token_generator import generate_token
 
 # Auth0 method to sign in user
 def auth_user(email, password):
-    """
-    Authenticates a user using email and password, generates a token, and creates a session.
-
-    Args:
-        email (str): User's email address for authentication.
-        password (str): User's password for authentication.
-
-    Returns:
-        jsonify: Returns a JSON response containing the generated token, user's admin status, user ID (uid),
-                 and verification status (verified).
-                 HTTP status code 200 for successful authentication, 401 for authentication failure,
-                 and 500 for internal server error.
-    """
     if check_user_credetials_in_database(email, password):
         token = generate_token()
         uid = get_user_by_email(email)
@@ -32,18 +19,6 @@ def auth_user(email, password):
     
 # Auth0 method to sign out user
 def unauth_user(uid, token):
-    """
-    Signs out a user by removing the session token.
-
-    Args:
-        uid (int): User ID (uid) for identifying the user.
-        token (str): Session token to be deleted.
-
-    Returns:
-        jsonify: Returns a JSON response indicating the success or failure of signing out.
-                 HTTP status code 200 for successful sign-out, 401 for invalid session token,
-                 and 500 for internal server error.
-    """
     if is_token_valid(token, uid):
         if delete_token(token):
             return jsonify({'data': "You have been signed out"}), 200
