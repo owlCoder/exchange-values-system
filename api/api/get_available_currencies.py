134a940a8a0427a7e5d7d246338819eaf0f4dfda
Danijel Jovanović <danijel.xda@gmail.com>
2023-11-29 15:27:16 +0100

    vercel

--- api/api/get_available_currencies.py
+++ api/api/get_available_currencies.py
@@ -2,10 +2,12 @@ from flask import json
 from http.server import BaseHTTPRequestHandler
 from services.currencies_service import get_available_currency
 
-class handler(BaseHTTPRequestHandler):
-    def currency(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
-        self.wfile.write((json.dumps(get_available_currency())).encode('utf-8'))
-        return
+def handler(request):
+    # Process the request
+    name = request.query.get('name') or 'Guest'
+
+    # Return the response
+    return {
+        'statusCode': 200,
+        'body': f'Hello, {name}!'
+    }
