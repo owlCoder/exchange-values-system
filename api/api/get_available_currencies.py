cb263c319c76d658558dbf1f8ccf9078f2aa4eb2
Danijel Jovanović <danijel.xda@gmail.com>
2023-11-29 15:30:41 +0100

    class

--- api/api/get_available_currencies.py
+++ api/api/get_available_currencies.py
@@ -1,13 +1,8 @@
-from flask import json
 from http.server import BaseHTTPRequestHandler
-from services.currencies_service import get_available_currency
 
-def handler(request):
-    # Process the request
-    name = request.query.get('name') or 'Guest'
-
-    # Return the response
-    return {
-        'statusCode': 200,
-        'body': f'Hello, {name}!'
-    }
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write('Hello, world!'.encode('utf-8'))
