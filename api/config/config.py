66195a6c6e095d500602aed871a65ce51f2901ef
Danijel Jovanović <danijel.xda@gmail.com>
2023-12-06 23:45:35 +0100

    Use local database
    Co-Authored-By: Vuk Ognjanovic <56590132+Vukajlo01@users.noreply.github.com>

--- api/config/config.py
+++ api/config/config.py
@@ -1,14 +1,9 @@
 # Database configuration
-# DB_USERNAME = 'root'
-# DB_PASSWORD = '123'
-# DB_HOST = '127.0.0.1'
-# DB_PORT = '3306'
-# DB_NAME = 'proddb'
-DB_USERNAME = 'avnadmin'
-DB_PASSWORD = 'AVNS_7v9isI8sV1NftpLJf-Y'
-DB_HOST = 'proddb-drs-wapi.a.aivencloud.com'
-DB_PORT = '28701'
-DB_NAME = 'defaultdb'
+DB_USERNAME = 'root'
+DB_PASSWORD = '123'
+DB_HOST = '127.0.0.1'
+DB_PORT = '3306'
+DB_NAME = 'proddb'
 
 # SQLAlchemy database URI
 SQLALCHEMY_DATABASE_URI = f'mysql+mysqlconnector://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?charset=utf8mb4'
